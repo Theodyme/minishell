8315a5aa00a1b274cfe087fc3a3ab2fd6f45066a
Theophane Place <flplace@e3r4p8.clusters.42paris.fr>
2023-03-15 16:27:50 +0100

    fixing merge again

--- ft_WIP/builtin/export.c
+++ ft_WIP/builtin/export.c
@@ -6,7 +6,7 @@
 /*   By: flplace <flplace@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 14:13:33 by flplace           #+#    #+#             */
-/*   Updated: 2023/03/15 16:23:58 by flplace          ###   ########.fr       */
+/*   Updated: 2023/03/15 16:27:26 by flplace          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,9 @@ int		ft_bltin_export(t_cmd *cmd)
 	char	*key;
 	char	*value;
 
-	key = ft_split_key(div->args[1]);
-	value = ft_split_value(div->args[1]);
-	tmp = ft_key_add(envt, key, value);
+	key = ft_split_key(cmd->args[1]);
+	value = ft_split_value(cmd->args[1]);
+	tmp = ft_key_add(cmd->envt, key, value);
 	if (ft_strcmp(tmp->key, key) == 0 && ft_strcmp(tmp->value, value) == 0)
 		return (0);
 	return (1);
