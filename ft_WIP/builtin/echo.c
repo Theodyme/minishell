759865b42dab9fb450660b5bb175280251ebf28d
m.bimich <manuel.bimich@gmail.com>
2023-04-03 15:53:52 +0200

    add: gestion de l'ecriture de echo lors de pipe

--- ft_WIP/builtin/echo.c
+++ ft_WIP/builtin/echo.c
@@ -6,7 +6,7 @@
 /*   By: mabimich <mabimich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 10:54:46 by flplace           #+#    #+#             */
-/*   Updated: 2023/04/03 14:57:46 by mabimich         ###   ########.fr       */
+/*   Updated: 2023/04/03 15:52:20 by mabimich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,13 @@
 
 int			ft_bltin_echo(t_cmd *cmd)
 {
-	int		out;
-
-	out = STDOUT_FILENO;
-	if (cmd->next)
-		out = cmd->next->fd[1];
-	ft_putendl_fd(cmd->argv[1], out);
-	return (0);
+	int stdout_copy;
+	
+	stdout_copy = dup(STDOUT_FILENO);
+	dup2(cmd->fd[1], STDOUT_FILENO);
+	if (cmd->argv && cmd->argv[1])
+		ft_putendl_fd(cmd->argv[1], 1);
+	dup2(stdout_copy, STDOUT_FILENO);
+    close(stdout_copy);
+	return (1);
 }
