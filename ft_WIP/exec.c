ed79ae6ad52a5fe8547fab8fc30aa062fb6b7c2e
Theophane Place <flplace@bess-f3r2s9.clusters.42paris.fr>
2023-07-26 18:03:07 +0200

    fixing merge - STABLE

--- ft_WIP/exec.c
+++ ft_WIP/exec.c
@@ -3,14 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mabimich <mabimich@student.42.fr>          +#+  +:+       +#+        */
+/*   By: flplace <flplace@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/22 17:28:34 by mabimich          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2023/07/26 17:44:13 by flplace          ###   ########.fr       */
-=======
-/*   Updated: 2023/07/26 17:48:37 by mabimich         ###   ########.fr       */
->>>>>>> refs/remotes/origin/master
+/*   Updated: 2023/07/26 18:01:59 by flplace          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -133,11 +129,8 @@ void	child(t_cmd *cmd)
 	}
 	if (path && cmd->argv && cmd->argv[0])
 		execve(path, cmd->argv, cmd->envp);
-<<<<<<< HEAD
 	// if (cmd->argv)
 	// 	ft_free_tab_str(cmd->argv, -1);
-=======
->>>>>>> refs/remotes/origin/master
 	ft_msg(cmd->name, "command not found");
 	// if (cmd->argv)
 	// 	ft_free_tab_str(cmd->argv, -1);
