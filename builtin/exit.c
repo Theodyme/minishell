160e51ee5ee859f83dae375b8b412ea34fb8b16c
Theophane Place <flplace@bess-f2r2s10.clusters.42paris.fr>
2023-10-24 19:25:02 +0200

    fix exit

--- builtin/exit.c
+++ builtin/exit.c
@@ -6,7 +6,7 @@
 /*   By: flplace <flplace@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 18:07:52 by flplace           #+#    #+#             */
-/*   Updated: 2023/10/16 16:35:43 by flplace          ###   ########.fr       */
+/*   Updated: 2023/10/24 19:23:59 by flplace          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,7 @@ int	ft_bltin_exit(t_cmd *cmd)
 
 	i = 0;
 	if (cmd->head != cmd || (cmd->next && cmd->next->args_list->str))
-		return (1);
+		return (0);
 	tmp = cmd->args_list->next;
 	if (!tmp->str)
 		ft_free_n_exit(cmd, 0);
