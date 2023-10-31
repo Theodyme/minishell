9e55ea07aa08037ab13345908b83f9b23b49ddfb
m.bimich <manuel.bimich@gmail.com>
2023-10-31 01:06:38 +0100

    multiple fix

--- init_child.c
+++ init_child.c
@@ -6,7 +6,7 @@
 /*   By: mabimich <mabimich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 17:25:43 by mabimich          #+#    #+#             */
-/*   Updated: 2023/10/30 21:02:47 by mabimich         ###   ########.fr       */
+/*   Updated: 2023/10/31 01:05:17 by mabimich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,31 +21,50 @@
 ** Enfin, on ouvre les pipes pour les processus enfants.
 */
 
+void	close_protection(t_cmd *cmd, t_redir *redir, int *tmp)
+{
+	if (tmp[0])
+	{
+		close(cmd->fd[0]);
+		cmd->fd[0] = tmp[0];
+	}
+	else if (tmp[1])
+	{
+		close(cmd->fd[1]);
+		cmd->fd[1] = tmp[1];
+	}
+	if (cmd->fd[0] == -1 || cmd->fd[1] == -1)
+	{
+		ft_msg(redir->file, strerror(errno));
+		close_pipes(cmd);
+		ft_free_n_exit(cmd->head, 1);
+	}
+}
+
 void	open_files(t_cmd *cmd)
 {
 	t_redir	*redir;
+	int		tmp[2];
 
+	tmp[0] = 0;
+	tmp[1] = 0;
 	redir = cmd->redir;
 	if (g_status == 130 || g_status == 131)
 		g_status = 0;
 	while (cmd && redir)
 	{
 		if (redir->type == REDIR_IN)
-			cmd->fd[0] = open(redir->file, O_RDONLY);
+			tmp[0] = open(redir->file, O_RDONLY);
 		else if (redir->type == HEREDOC)
-			cmd->fd[0] = here_doc(redir);
+			tmp[0] = here_doc(redir);
 		else if (redir->type == REDIR_OUT)
-			cmd->fd[1] = open(redir->file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+			tmp[1] = open(redir->file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 		else if (redir->type == APPEND)
-			cmd->fd[1] = open(redir->file, O_WRONLY | O_CREAT | O_APPEND, 0644);
-		if (cmd->fd[0] == -1 || cmd->fd[1] == -1)
-		{
-			ft_msg(redir->file, strerror(errno));
-			ft_free_n_exit(cmd->head, 1);
-		}
+			tmp[1] = open(redir->file, O_WRONLY | O_CREAT | O_APPEND, 0644);
+		close_protection(cmd, redir, tmp);
 		redir = redir->next;
 	}
-	if (g_status == 131 || g_status == 130)
+	if (g_status == 130 || g_status == 131)
 		dispatch_exit(cmd, g_status);
 }
 
@@ -80,10 +99,10 @@ void	close_pipes(t_cmd *cmd)
 	tmp = cmd->head;
 	while (tmp)
 	{
-		if (tmp->fd[0] != STDIN_FILENO)
+		if (tmp->fd[0] != STDIN_FILENO && tmp->fd[0] != -1)
 			close(tmp->fd[0]);
-		if (tmp->fd[1] != STDOUT_FILENO)
-			close(tmp->fd[1]);
+		if (tmp->fd[1] != STDOUT_FILENO && tmp->fd[1] != -1)
+			close (tmp->fd[1]);
 		tmp = tmp->next;
 	}
 }
